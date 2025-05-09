78e8f9c521a305aa823a6bfed4f66bcf313dde32
karolyne marques <karoldm1579@gmail.com>
2025-05-09 16:10:12 -0300

    feat: add icons, mask text input formatters, cached network image and use icons in circular avatar widget

--- bookstore/lib/ui/_core/widgets/circular_avatar_widget.dart
+++ bookstore/lib/ui/_core/widgets/circular_avatar_widget.dart
@@ -1,46 +1,50 @@
-import 'dart:convert';
-
+import 'package:bookstore/ui/_core/icons/book_store_icons_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:bookstore/ui/_core/theme/app_colors.dart';
 
-class CircleAvatarWidget extends StatelessWidget {
-  final String? image;
-  final String? name;
-  final double radius;
+final iconsAvatar = [
+  BookStoreIcons.cat_1,
+  BookStoreIcons.cat_2,
+  BookStoreIcons.cat_3,
+  BookStoreIcons.cat_4,
+  BookStoreIcons.cat_5,
+  BookStoreIcons.cat_6,
+  BookStoreIcons.cat_7,
+  BookStoreIcons.cat_8,
+  BookStoreIcons.cat_9,
+];
 
-  const CircleAvatarWidget({
-    super.key,
-    this.image,
-    this.name,
-    this.radius = 64,
-  });
+class CircleAvatarWidget extends StatelessWidget {
+  final String? name;
+  final double iconSize;
+
+  const CircleAvatarWidget({super.key, this.name, this.iconSize = 80});
 
   @override
   Widget build(BuildContext context) {
-    final names = name?.split(" ");
-    String content = "";
-    if (names != null) {
-      if (names.length > 1 && names[0].isNotEmpty && names[1].isNotEmpty) {
-        content = names[0][0] + names[1][0];
-      } else if (names.length == 1) {
-        content = names[0].isEmpty ? "" : names[0][0];
-      }
-    }
+    int index =
+        name == null || name!.trim() == ""
+            ? 0
+            : name!
+                    .trim()
+                    .split("")
+                    .map((e) {
+                      return e.codeUnitAt(0);
+                    })
+                    .reduce((a, b) => a + b) %
+                iconsAvatar.length;
 
-    return CircleAvatar(
-      radius: radius,
-      backgroundColor: AppColors.defaultColor,
-      backgroundImage: image != null ? MemoryImage(base64Decode(image!)) : null,
-      child:
-          image == null
-              ? Text(
-                content.toUpperCase(),
-                style: TextStyle(
-                  color: AppColors.backgroundColor,
-                  fontSize: radius * 0.5,
-                ),
-              )
-              : null,
+    return Container(
+      padding: EdgeInsets.all(0.3 * iconSize),
+      decoration: BoxDecoration(
+        color: AppColors.inputColor,
+        shape: BoxShape.circle,
+      ),
+      child: Icon(
+        iconsAvatar[index],
+        size: iconSize,
+        color: AppColors.defaultColor,
+      ),
     );
   }
 }

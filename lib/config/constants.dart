b6fc1eaeebf923fc189c7310532d724f5422fc20
karolyne marques <karoldm1579@gmail.com>
2025-05-11 17:32:12 -0300

    fix: hard url

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,3 @@
 const String userSessionKey = "USER_SESSION";
-const String apiUrl = String.fromEnvironment('BASE_URL');
+const String apiUrl = "https://bookstore-api-0pfv.onrender.com";
 const String savedBooksKey = "SAVED_BOOKS";
